8966a5295a4ee0e566dc953d789061318d57215a
BurntRanch <69512353+BurntRanch@users.noreply.github.com>
2025-01-07 16:13:52 +0300

    You can now specify which server to connect to through the command args!

--- shaders/lighting.frag
+++ shaders/lighting.frag
@@ -8,5 +8,5 @@ layout(binding = 1) uniform sampler2D tex;
 layout(location = 0) out vec4 outColor;
 
 void main() {
-    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    outColor = texture(tex, fragCoord);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "camera.hpp"
+#include <SDL3/SDL_events.h>
 #include <SDL3/SDL_keycode.h>
 #include <SDL3/SDL_mouse.h>
 #include <SDL3/SDL_scancode.h>
@@ -29,47 +30,108 @@
 #include "rapidxml_print.hpp"
 #include "tinyfiledialogs.h"
 
-Camera cam(glm::vec3(0.0f, 0.0f, 0.0f));
 Settings settings("settings.toml");
 std::unique_ptr<Engine> engine;
 
 namespace State {
+    /* The direction to move along the X axis. */
+    float Movement;
+
+    std::string ServerAddress;
+
+    enum packetType {
+        PACKET_MOVEMENT_REQUEST,
+    };
+
+    struct Packet {
+        packetType type;
+
+        /* if packetType == PACKET_MOVEMENT_REQUEST*/
+        float movementDirection;
+    };
+
+    Packet lastPacket;
 }
 
 float lastX, lastY;
 Uint32 lastMouseState;
+
+void SendPacketToServer(State::Packet &packet) {
+    UTILASSERT(engine->IsConnectedToGameServer());
+
+    std::vector<std::byte> data;
+    Serialize(static_cast<int>(packet.type), data);
+    Serialize(packet.movementDirection, data);
+
+    engine->SendRequestToServer(data);
+}
 
 void Update() {
     //fmt::println("Hi!");
 }
 
 void FixedUpdate(const std::array<bool, 322> &keyMap) {
+    if (!engine->IsConnectedToGameServer()) {
+        return;
+    }
 
+    State::Movement = 0;
+
+    if (keyMap[SDL_SCANCODE_RIGHT]) {
+        fmt::println("Moving right!");
+        State::Movement += 1.0f / 64.0f;
+    }
+
+    if (keyMap[SDL_SCANCODE_LEFT]) {
+        fmt::println("Moving left");
+        State::Movement -= 1.0f / 64.0f;
+    }
+
+    State::Packet packet{};
+    packet.type = State::PACKET_MOVEMENT_REQUEST;
+    packet.movementDirection = State::Movement;
+
+    if (packet.movementDirection != State::lastPacket.movementDirection) {
+        SendPacketToServer(packet);
+
+        State::lastPacket = packet;
+    }
 }
 
 void onButtonClick(std::string id) {
     if (id == "connectButton") {
         SteamNetworkingIPAddr ipAddr;
         ipAddr.Clear();
-        ipAddr.ParseString("127.0.0.1:9582");
+
+        if (!State::ServerAddress.empty()) {
+            ipAddr.ParseString(State::ServerAddress.c_str());
+            
+            if (ipAddr.m_port == 0) {
+                ipAddr.m_port = 9582;
+            }
+        } else {
+           ipAddr.ParseString("127.0.0.1:9582");
+        }
 
         engine->ConnectToGameServer(ipAddr);
     }
 }
 
-int main() {
-    cam.MouseSensitivity = settings.MouseSensitivity;
-    cam.FOV = settings.FieldOfView;
-    cam.MovementSpeed = settings.Velocity;
-
+int main(int argc, char *argv[]) {
     engine = std::make_unique<Engine>();
     
-    engine->InitRenderer(settings, &cam);
+    engine->InitRenderer(settings, nullptr);
     engine->InitNetworking();
+
+    engine->GetRenderer()->RegisterFixedUpdateFunction(FixedUpdate);
 
     engine->RegisterUIButtonListener(onButtonClick);
 
     engine->LoadUIFile("ui.xml");
+
+    if (argc >= 2) {
+        State::ServerAddress = argv[1];
+    }
 
     engine->StartRenderer();
 
